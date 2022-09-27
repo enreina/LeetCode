1916aa9cd80b533e166acf6417c3ff4c1cb6067f
Enreina <enreina@gmail.com>
2022-09-27 16:17:58 +0700

    Time: 136 ms (41.69%), Space: 44.4 MB (72.07%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.js
+++ 724-find-pivot-index/724-find-pivot-index.js
@@ -1,28 +1,16 @@
 /**
  * @param {number[]} nums
  * @return {number}
- */
+*/
+
+// more optimal solution by only calculating the sum of the left side
 var pivotIndex = function(nums) {
-    // One approach is to have two pointer and sum: 
-    // - first one moves from left to right
-    // - second one moves from right to left
-    // Second approach is to loop from left to right for the pivot index
-    // and then sum the left side and right side
-    // check if the sum is equal; if equal returns the index
-    // if not equal, increase the pivot index
+    let sum = 0; leftSum = 0;
+    nums.forEach((element) => sum += element);
     for (let i = 0; i < nums.length; i++) {
-        let leftSide = nums.slice(0, i);
-        let rightSide = nums.slice(i+1, nums.length);
-        let sumLeft = 0;
-        leftSide.forEach((element) => {
-            sumLeft += element;
-        });
-        let sumRight = 0;
-        rightSide.forEach((element) => {
-            sumRight += element
-        });
-        if (sumLeft == sumRight) return i;
+        let rightSum = sum - leftSum - nums[i];
+        if (rightSum == leftSum) return i;
+        leftSum += nums[i];
     }
-    
     return -1;
-};
+}
