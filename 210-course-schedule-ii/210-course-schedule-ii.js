ad58aa852137d35151037c61688b1bd011e8de90
Enreina <enreina@gmail.com>
2022-10-06 08:11:28 +0700

    Time: 110 ms (76.08%), Space: 48 MB (45.07%) - LeetHub

---
+++ 210-course-schedule-ii/210-course-schedule-ii.js
@@ -0,0 +1,50 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {number[]}
+ */
+var findOrder = function(numCourses, prerequisites) {
+    // topological ordering
+    // build the graph
+    const preqGraph = [];
+    for (let ii=0; ii<numCourses; ii++) {
+        preqGraph[ii] = [];
+    }
+    prerequisites.forEach(([course, preq]) => {
+        preqGraph[preq].push(course);
+    });
+    // console.log(preqGraph);
+    // keep track of visited course
+    const visitedCourse = new Set();
+    const beingVisited = new Set();
+    const sortedCourse = [];
+    let cycleDetected = false;
+    const dfs = (currentCourse, sortedCourse) => {
+        // console.log(currentCourse);
+        // mark as visited
+        visitedCourse.add(currentCourse);
+        beingVisited.add(currentCourse);
+        // console.log(visitedCourse);
+        // visit neightbors (courses which has this currentCourse as a prerequisite)
+        preqGraph[currentCourse].forEach((course) => {
+            if (beingVisited.has(course)) {
+                cycleDetected = true;
+                // console.log("cycle detected");
+            }
+            if (!visitedCourse.has(course)) {
+                dfs(course, sortedCourse);
+            }
+        });
+        sortedCourse.unshift(currentCourse);
+        beingVisited.delete(currentCourse);
+        // console.log(sortedCourse);
+    }
+    // run dfs on every unvisited course
+    preqGraph.forEach((_, course) => {
+        if (!visitedCourse.has(course)) {
+            dfs(course, sortedCourse);
+        }
+    });
+    
+    return !cycleDetected ? sortedCourse : [];
+};
