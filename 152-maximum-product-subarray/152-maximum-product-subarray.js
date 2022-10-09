54ed4f950eaad0d039dae9f992178b70cd3418e0
Enreina <enreina@gmail.com>
2022-10-09 08:34:03 +0700

    Time: 110 ms (40.31%), Space: 44.1 MB (33.83%) - LeetHub

---
+++ 152-maximum-product-subarray/152-maximum-product-subarray.js
@@ -0,0 +1,59 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxProduct = function(nums) {
+    // We keep track of the max product for n elements, which is between
+    //  - The maximum from n-1 elements * the nth number
+    //  - The minimum from n-1 elements * the nth number (this is in case a negative number times another negative can be the a maximum candidate)
+    //  - The nth number itself
+    // We keep track of th min product for n elements which is between all the above candidate as well
+    const maxProduct = new Array(nums.length);
+    const minProduct = new Array(nums.length);
+    
+    maxProduct[0] = nums[0];
+    minProduct[0] = nums[0];
+    let result = nums[0];
+    
+    for (let n=1; n<nums.length; n++) {
+        maxProduct[n] = Math.max(maxProduct[n-1] * nums[n], minProduct[n-1] * nums[n], nums[n]);
+        minProduct[n] = Math.min(maxProduct[n-1] * nums[n], minProduct[n-1] * nums[n], nums[n]);
+        result = Math.max(result, maxProduct[n]);
+    }
+    
+    return Math.max(result, maxProduct[nums.length-1]);
+};
+// The following solution was attempted but got Time Limit Exceeded as it has worst time complexity of O(n^2)
+var maxProductTLE = function(nums) {
+    // maxProductDP[0] => nums has one element, max product will be nums[0]
+    // maxProductDP[1] => nums has two element, max product will be between nums[0], or nums[1], or nums[0] * nums[1]
+    // maxProductDP[2] => nums has three elements, max product will be between
+    //  - nums[0]
+    //  - nums[1]
+    //  - nums[0] * nums[1]
+    //      the three above is equal to maxProductDP[1]
+    //  - nums[2]
+    //  - nums[1] * nums[2]
+    //  - nums[0] * nums[1] * nums[2]
+    // nums has four elements, max product will be between
+    //  - maxProductDP[2]
+    //  - nums[3]
+    //  - nums[2] * nums[3]
+    //  - nums[1] * nums[2] * nums[3]
+    //  - nums[0] * nums[1] * nums[2] * nums[3]
+    
+    const maxProductDP = new Array(nums.length);
+    maxProductDP[0] = nums[0];
+    const accumulativeProduct = new Array(nums.length);
+    
+    for (let n=1; n<nums.length; n++) {
+        maxProductDP[n] = maxProductDP[n-1];
+        let product = 1;
+        for (let ii=n; ii>=0; ii--) {
+            product = product * nums[ii];
+            maxProductDP[n] = Math.max(maxProductDP[n], product);
+        }
+    }
+    
+    return maxProductDP[nums.length - 1];
+};
