01758b842965bf1594d394cf35d6431adc4adaea
Enreina <enreina@gmail.com>
2022-10-13 15:32:58 +0700

    Time: 208 ms (7.79%), Space: 46.9 MB (68.07%) - LeetHub

---
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.js
@@ -0,0 +1,83 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var calculate = function(s) {
+    const operatorStack = [];
+    const operandStack = [];
+    const Precedence = {
+        '+': 0,
+        '-': 0,
+        '*': 1,
+        '/': 1,
+    };
+    
+    const evaluate = (firstOperand, secondOperand, operator) => {
+        if (operator === "+") {
+            return firstOperand + secondOperand;
+        } else if (operator === "-") {
+            return firstOperand - secondOperand;
+        } else if (operator === "*") {
+            return firstOperand * secondOperand;
+        } else if (operator === "/") {
+            return Math.floor(firstOperand / secondOperand);
+        }
+    }
+    
+    // evaluate expression
+    let currentNumberStr = "";
+    for (let ii=0; ii<s.length; ii++) {
+        const currentChar = s.charAt(ii);
+        if (!Number.isNaN(Number.parseInt(currentChar))) {
+            currentNumberStr = `${currentNumberStr}${currentChar}`;
+        } else {
+            // we push the previous number (operand) to stack; handling multiple digits
+            const currentNumber = Number.parseInt(currentNumberStr);
+            if (!Number.isNaN(currentNumber)) {
+                operandStack.push(currentNumber);
+                currentNumberStr = "";
+            }
+            
+            if (["+", "-", "*", "/"].includes(currentChar)) {
+                // if stack is empty, push the operator into the stack
+                if (operatorStack.length === 0) {
+                    operatorStack.push(currentChar);
+                } else {
+                    const topOfStack = operatorStack.at(-1);
+                    // if operator precedence is greater than precendence of top of operator stack, push the operator into the stack
+                    if (Precedence[currentChar] > Precedence[topOfStack]) {
+                        operatorStack.push(currentChar);
+                    } else {
+                        // otherwise we pop and evaluate until we found an operator with lower or equal precedence
+                        let currentOperator = operatorStack.at(-1);
+                        while (Precedence[currentChar] <= Precedence[currentOperator] && operatorStack.length > 0) {
+                            operatorStack.pop();
+                            const secondOperand = operandStack.pop();
+                            const firstOperand = operandStack.pop();
+                            const evaluationResult = evaluate(firstOperand, secondOperand, currentOperator);
+                            operandStack.push(evaluationResult);
+                            currentOperator = operatorStack.at(-1);
+                        }
+                        operatorStack.push(currentChar);
+                    }
+                }
+                
+            }
+        }
+    }
+    const currentNumber = Number.parseInt(currentNumberStr);
+    if (!Number.isNaN(currentNumber)) {
+        operandStack.push(currentNumber);
+        currentNumberStr = "";
+    }
+    
+    // now we process the remaining operators & operand
+    while (operatorStack.length > 0) {
+        const currentOperator = operatorStack.pop();
+        const secondOperand = operandStack.pop();
+        const firstOperand = operandStack.pop();
+        operandStack.push(evaluate(firstOperand, secondOperand, currentOperator));
+    }
+    
+    return operandStack.pop();
+};
