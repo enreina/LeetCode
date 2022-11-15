b976c8859e66e8530b8d377fe8d807b7feb880be
Enreina <enreina@gmail.com>
2022-11-15 14:08:55 +0700

    Time: 166 ms (73.05%), Space: 48.2 MB (89.25%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.js
@@ -3,10 +3,29 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
+    // non-trivial solution: two pointer approach
+    const result = new Array(nums.length);
+    let leftPointer = 0;
+    let rightPointer = nums.length - 1;
+    let currentPointer = rightPointer;
+    while (leftPointer <= rightPointer) {
+        if (nums[leftPointer] ** 2 > nums[rightPointer] ** 2) {
+            result[currentPointer] = nums[leftPointer] ** 2;
+            currentPointer--;
+            leftPointer++;
+        } else {
+            result[currentPointer] = nums[rightPointer] ** 2;
+            currentPointer--;
+            rightPointer--;
+        }
+    }
+    
+    return result;
+    
     // trivial solution
     // square each number
-    const squaredNums = nums.map(x => x * x);
-    // sort the new array
-    squaredNums.sort((a, b) => a - b);
-    return squaredNums;
+    // const squaredNums = nums.map(x => x * x);
+    // // sort the new array
+    // squaredNums.sort((a, b) => a - b);
+    // return squaredNums;
 };
