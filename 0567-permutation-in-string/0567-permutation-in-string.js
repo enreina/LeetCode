524e3ed5ead179adb5383f63c3cbdc6eb9ff7155
Enreina <enreina@gmail.com>
2022-11-18 17:56:39 +0700

    Time: 410 ms (25.84%), Space: 51.5 MB (9.80%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.js
+++ 0567-permutation-in-string/0567-permutation-in-string.js
@@ -19,17 +19,28 @@ var checkInclusion = function(s1, s2) {
     // with the length of s1 as k, move a k-size sliding window on s2 and check if occurences of chars is the as with s1
     let leftPointer = 0;
     let rightPointer = leftPointer + s1.length;
-    while (rightPointer <= s2.length) {
-        const s2Characters = {};
-        for (let idx=leftPointer; idx<rightPointer; idx++) {
-            const currentChar = s2.charAt(idx);
-            s2Characters[currentChar] = s2Characters[currentChar] ? s2Characters[currentChar] + 1 : 1;
+    const s2Characters = {};
+    for (let idx=leftPointer; idx<rightPointer; idx++) {
+        const currentChar = s2.charAt(idx);
+        s2Characters[currentChar] = s2Characters[currentChar] ? s2Characters[currentChar] + 1 : 1;
+    }
+    if (_.isEqual(s1Characters, s2Characters)) {
+        return true;
+    }
+    while (rightPointer < s2.length) {
+        leftPointer++;
+        rightPointer++;
+        const prevChar = s2.charAt(leftPointer-1);
+        if (s2Characters[prevChar] - 1 == 0) {
+            delete s2Characters[prevChar];
+        } else {
+            s2Characters[prevChar] = s2Characters[prevChar] - 1;
         }
+        const newChar = s2.charAt(rightPointer-1);
+        s2Characters[newChar] = s2Characters[newChar] ? s2Characters[newChar] + 1 : 1;
         if (_.isEqual(s1Characters, s2Characters)) {
             return true;
         }
-        leftPointer++;
-        rightPointer++;
     }
     return false;
 };
