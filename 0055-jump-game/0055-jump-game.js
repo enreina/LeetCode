46675cce6950e2f0b8948da42543a58bb1bf3465
Enreina <enreina@gmail.com>
2022-12-23 11:42:44 +0700

    Time: 87 ms (83.49%), Space: 46.2 MB (73.61%) - LeetHub

--- 0055-jump-game/0055-jump-game.js
+++ 0055-jump-game/0055-jump-game.js
@@ -4,17 +4,33 @@
  */
 var canJump = function(nums) {
     // for each num we mark the indexes that we can jump to
-    const canJump = Array(nums.length);
-    canJump.fill(false);
-    canJump[0] = true;
+    // complexity O(n^2)
+    // const canJump = Array(nums.length);
+    // canJump.fill(false);
+    // canJump[0] = true;
+    
+    // nums.forEach((currentMaxJump, currentIdx) => {
+    //     if (canJump[currentIdx]) {
+    //         for (let idx=currentIdx+1; idx<=Math.min(currentIdx+currentMaxJump, nums.length - 1); idx++) {
+    //             canJump[idx] = true;
+    //         }
+    //     }
+    // });
+    // 
+    // return canJump[nums.length - 1];
+
+    // Greedy: keep track of reachable maximum index
+    // Time complexity: O(n)
+    let maxIdx = 0;
     nums.forEach((currentMaxJump, currentIdx) => {
-        if (canJump[currentIdx]) {
-            for (let idx=currentIdx+1; idx<=Math.min(currentIdx+currentMaxJump, nums.length - 1); idx++) {
-                canJump[idx] = true;
-            }
+        if (currentIdx > maxIdx) {
+            maxIdx = 0; // we reset max index if the current position is unreachable 
+        } else {
+            maxIdx = Math.max(maxIdx, currentIdx+currentMaxJump);
         }
     });
-    
 
-    return canJump[nums.length - 1];
+    return maxIdx >= nums.length - 1; 
+
+    
 };
