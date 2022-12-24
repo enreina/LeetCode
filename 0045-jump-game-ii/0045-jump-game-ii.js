69d6ec136839c8ddf966dbecddf49b9c27916046
Enreina <enreina@gmail.com>
2022-12-24 08:10:15 +0700

    Time: 81 ms (83.41%), Space: 43.8 MB (76.47%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.js
+++ 0045-jump-game-ii/0045-jump-game-ii.js
@@ -3,6 +3,24 @@
  * @return {number}
  */
 var jump = function(nums) {
+    // Greedy Approach
+    // [2, 3, 1, 1, 4]
+    // reachable from 0 = [2, 3, 1]
+    // reachable from 1 = [3, 1, 1, 4]
+
+    let minimumJump = 0;
+    let maxReachableIdx = 0; 
+    let nextJumpIdx = 0;
+    for (let idx = 0; idx < nums.length - 1; idx++) {
+        maxReachableIdx = Math.max(maxReachableIdx, idx + nums[idx]);
+        if (idx === nextJumpIdx) {
+            minimumJump++;
+            nextJumpIdx = maxReachableIdx;
+        }
+    }
+    return minimumJump;
+
+    // DP Approach
     // [2, 3, 1, 1, 4]
     // walk backwards from last index
     // [4] -> minimum jump = 0
@@ -17,17 +35,17 @@ var jump = function(nums) {
     //                      1 + minimum jump of ([1, 1, 4]) = 3
     //                      1 + minimum jump of ([3, 1, 1, 4]) = 2
 
-    const minimumJump = Array(nums.length);
-    minimumJump[nums.length - 1] = 0;
-    for (let startIdx = nums.length - 2; startIdx>=0; startIdx--) {
-        minimumJump[startIdx] = Number.POSITIVE_INFINITY;
-        let nextIdx = Math.min(startIdx + nums[startIdx], nums.length - 1);
-        while (nextIdx > startIdx) {
-            minimumJump[startIdx] = Math.min(minimumJump[startIdx], 1 + minimumJump[nextIdx]);
-            nextIdx--;
-        }
+    // const minimumJump = Array(nums.length);
+    // minimumJump[nums.length - 1] = 0;
+    // for (let startIdx = nums.length - 2; startIdx>=0; startIdx--) {
+    //     minimumJump[startIdx] = Number.POSITIVE_INFINITY;
+    //     let nextIdx = Math.min(startIdx + nums[startIdx], nums.length - 1);
+    //     while (nextIdx > startIdx) {
+    //         minimumJump[startIdx] = Math.min(minimumJump[startIdx], 1 + minimumJump[nextIdx]);
+    //         nextIdx--;
+    //     }
         
-    }
+    // }
 
-    return minimumJump[0];
+    // return minimumJump[0];
 };
