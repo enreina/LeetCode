3f25d63ebc42c347490e329d70d4907ed9f63e47
Enreina <enreina@gmail.com>
2023-01-03 14:03:44 +0700

    Time: 106 ms (63.55%), Space: 46.3 MB (51.40%) - LeetHub

---
+++ 0944-delete-columns-to-make-sorted/0944-delete-columns-to-make-sorted.js
@@ -0,0 +1,21 @@
+/**
+ * @param {string[]} strs
+ * @return {number}
+ */
+var minDeletionSize = function(strs) {
+    let colToBeDeleted = 0;
+    // loop through each column, add colToBeDeleted if it's not lexicographically sorted
+    for (let colIdx=0; colIdx<strs[0].length; colIdx++) {
+        for (let rowIdx=0; rowIdx<strs.length-1; rowIdx++) {
+            const currentLetter = strs[rowIdx].charAt(colIdx);
+            const nextLetter = strs[rowIdx+1].charAt(colIdx);
+            if (currentLetter > nextLetter) {
+                colToBeDeleted++;
+                break;
+            }
+        }
+    }
+    return colToBeDeleted;
+    // Time complexity: O(N * K) -> number of strings times length of each string
+    // Space complexity: O(1) -> we only need to store colToBeDeleted
+};
